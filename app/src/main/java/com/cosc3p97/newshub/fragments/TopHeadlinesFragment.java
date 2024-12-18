a4b87e3cac8accc532d3a48afdb4ac8f18adc8a8
Yuvraj <17yuvraj.sehgal@gmail.com>
2024-12-18 18:54:22 -0500

    cleanup

--- app/src/main/java/com/cosc3p97/newshub/fragments/TopHeadlinesFragment.java
+++
@@ -1,8 +0,0 @@
-package com.cosc3p97.newshub.fragments;
-
-import com.cosc3p97.newshub.Constants;
-
-public abstract class TopHeadlinesFragment {
-    private static final String API_KEY = Constants.API_KEY;
-    private String country = "";
-}
